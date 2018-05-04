253735657b1085c78c87babe3ef5eda463384383
Patrick Fonseca <patrick.fcf@gmail.com>
2018-05-04 20:00:54 +0100

    About us page
    About us page
     - PT and EN
    
    Co-Authored-By: catiamatias8 <catiamatias8@users.noreply.github.com>
    Co-Authored-By: SofiaFG <sofiafg@users.noreply.github.com>

--- beta/about-us.php
+++ beta/about-us.php
@@ -6,6 +6,12 @@
   do_unirent_header('Sobre - UniRent');
 ?>
 
+<style>
+	div.a {
+	    text-indent: 50px;
+	}
+</style>
+
 
 <!-- PAGE TITLE SECTION -->
 <section class="clearfix pageTitleSection" style="background-image: url();">
@@ -21,67 +27,131 @@
 </section>
 
 
-<!-- HOW WORKS SECTION -->
-<section class="clearfix howWorksSection">
+<!-- ABOUT US PAGE SECTION -->
+<section class="clearfix termsInfoSection">
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12">
-				<div class="howWorksInner">
-					<div class="row">
-						<div class="col-sm-4 col-sm-push-8 col-xs-12">
-							<div class="howWorksImage text-right"><img src="img/works/Need.png" width="250" height="250" alt="Image Works"></div>
-						</div>
-						<div class="col-sm-8 col-sm-pull-4 col-xs-12">
-							<div class="howWorksInfo text-left">
-								<p>1º passo</p>
-								<h2>Necessidade</h2>
-								<p>Precisas de um bem, e a melhor maneira é alugar em vez de comprar.</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="howWorksInner">
-					<div class="row">
-						<div class="col-sm-4 col-xs-12">
-							<div class="howWorksImage text-left"><img src="img/works/Search.png" width="250" height="250" alt="Image Works"></div>
-						</div>
-						<div class="col-sm-8 col-xs-12">
-							<div class="howWorksInfo text-right">
-								<p>2º Passo</p>
-								<h2>Pesquisa</h2>
-								<p>Procura, na nossa plataforma, por qualquer bem que gostarias de alugar. Também podes fazer um pedido de um bem quando precisares dele.</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="howWorksInner">
-					<div class="row">
-						<div class="col-sm-4 col-sm-push-8 col-xs-12">
-							<div class="howWorksImage text-right"><img src="img/works/Rent.png" width="250" height="250" alt="Image Works"></div>
-						</div>
-						<div class="col-sm-8 col-sm-pull-4 col-xs-12">
-							<div class="howWorksInfo text-left">
-								<p>3º Passo</p>
-								<h2>Aluga</h2>
-								<p>Aluga qualquer bem que precises, diretamente com o <u>Owner</u>, de maneira simples e eficaz.</p>
-							</div>
-						</div>
-					</div>
-				</div>
-        <div class="howWorksInner">
-          <div class="row">
-            <div class="col-sm-4 col-xs-12">
-              <div class="howWorksImage text-left"><img src="img/works/Return.png" width="250" height="250" alt="Image Works"></div>
-            </div>
-            <div class="col-sm-8 col-xs-12">
-              <div class="howWorksInfo text-right">
-                <p>4º Passo</p>
-                <h2>Devolve</h2>
-                <p>Devolve o bem que alugaste ao seu <u>Owner</u>, não esquecendo de o entregar num horário conveniente. </p>
-              </div>
-            </div>
-          </div>
-        </div>
+				<ul class="list-unstyled termsList">
+					<li>
+						<div class="a">
+						<p align="justify">A UniRent nasceu através da ideia de uma estudante portuguesa que frequentou o programa Erasmus na Alemanha. Ana Rodrigues, passou 1 ano da sua vida de estudante universitária a estudar na Alemanha. Estando deslocada da sua residência habitual, necessitou de alguns objetos que a sua residência universitária não disponibilizava. Como um mini frigorífico, roupa de cama, objetos de decoração, instrumentos musicais ou uma bicicleta, tendo a necessidade e não existindo muitas opções para a sua obtenção, a Ana decidiu investir na compra dos bens, o problema surgiu quando regressou a Portugal e não tinha como trazer esses bens consigo. Os bens acabaram por ficar na Alemanha, pois para além de não os conseguir trazer consigo também não os conseguiu rentabilizar.</p></div>
+						<div class="a"> 
+						<br><p align="justify">Assim, a Ana pensou numa solução que poderia resolver o problema de tantos outros estudantes que enfrentam o mesmo problema que ela enfrentou. Surge então a ideia de uma plataforma de aluguer de bens por um curto período de tempo, com preços, ofertas e funcionalidades direcionadas a estudantes. O Mercado de Aluguer de Estudantes ou The Student’s Renting Marketplace, a UniRent. Aqui, os estudantes vão poder encontrar os bens que precisam, mas com os quais não querem dispender dinheiro desnecessário. Por outro lado, vão também poder rentabilizar os bens que não utilizam, por estarem longe deles fisicamente ou por, simplesmente, não lhes darem utilidade no momento presente.</p></div> 
+						<div class="a">
+						<br><p align="justify">A Ana conta agora com 4 colegas empenhados que a vão ajudar a levar a UniRent para a frente. No total, somos 5 alunos do Mestrado de Gestão de Sistemas de Informação da Instituto Superior de Economia e Gestão no entanto, temos percursos distintos. Passando então a uma breve apresentação de cada um.</p></div>
+						<div class="a">
+						<br><p align="justify">Ana Rodrigues, portuguesa, estudou Gestão de Marketing no ISCTE, em Lisboa, adora viajar e conhecer novas culturas e pessoas.</p></div>
+						<div class="a">
+						<br><p>Cátia Matias, portuguesa, estudou Gestão no ISEG, em Lisboa, gosta de novas tecnologias e considera-se uma bookworm por natureza.</p></div>
+						<div class="a">
+						<br><p align="justify">Patrick Fonseca, brasileiro, estudou Sistemas de Informação na Mackenzie, em São Paulo, entusiasta das novas tecnologias com uma enorme vontade de conhecer o mundo.</p>
+						<div class="a">
+						<br><p align="justify">Rui Varela, português, estudou Gestão no ISEG, em Lisboa, gosta de desporto e está sempre a par do que acontece na sociedade.</p></div>
+						<div class="a">
+						<br><p align="justify">Sofia Gonçalves, portuguesa, estudou Matemática Aplicada na FCUL, em Lisboa, tem um talento natural para tudo o que envolva números e espera um dia viajar pelo mundo.</p></div><br><br><br>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12">
+			<div id="thubmnailSlider" class="carousel slide thumbnailCarousel">
+			  <ol class="carousel-indicators">
+			    <li data-target="#thubmnailSlider" data-slide-to="0" class="active"></li>
+			    <li data-target="#thubmnailSlider" data-slide-to="1"></li>
+			    <li data-target="#thubmnailSlider" data-slide-to="2"></li>
+			    <li data-target="#thubmnailSlider" data-slide-to="3"></li>
+			    <li data-target="#thubmnailSlider" data-slide-to="4"></li>
+			    <li data-target="#thubmnailSlider" data-slide-to="5"></li>
+			  </ol>
+
+			  <!-- Carousel items -->
+			  <div class="carousel-inner">
+			    <div class="item row active">
+			      <div class="col-md-4 col-sm-6 col-xs-12">
+			        <div class="thingsBox">
+			          <div class="thingsImage">
+			            <img src="img/team/ana.jpeg" alt="Ana Rodrigues">
+			          </div>
+			          <div class="thingsCaption">
+			            <ul class="list-inline captionItem">
+			              <li><i class="fa" aria-hidden="true"></i> Ana Rodrigues</li>
+			              <li><a href="https://www.linkedin.com/in/aropio/" target="_blank">LinkedIn</a></li>
+			            </ul>
+			          </div>
+			        </div>
+			      </div>
+			    </div>
+
+			    <div class="item row">
+			      <div class="col-md-4 col-sm-6 col-xs-12">
+			        <div class="thingsBox">
+			          <div class="thingsImage">
+			            <img src="img/team/catia.png" alt="Cátia Matias" height="350" width="500">
+			          </div>
+			          <div class="thingsCaption">
+			            <ul class="list-inline captionItem">
+			              <li><i class="fa" aria-hidden="true"></i> Cátia Matias</li>
+			              <li><a href="https://www.linkedin.com/in/catiamatias/" target="_blank">LinkedIn</a></li>
+			            </ul>
+			          </div>
+			        </div>
+			      </div>
+			    </div>
+
+			    <div class="item row">
+			      <div class="col-md-4 col-sm-6 col-xs-12">
+			        <div class="thingsBox">
+			          <div class="thingsImage">
+			            <img src="img/team/patrick.jpg" alt="Patrick Fonseca" height="350" width="500">
+			          </div>
+			          <div class="thingsCaption">
+			            <ul class="list-inline captionItem">
+			              <li><i class="fa" aria-hidden="true"></i> Patrick Fonseca</li>
+			              <li><a href="https://www.linkedin.com/in/patrickfcf/" target="_blank">LinkedIn</a></li>
+			            </ul>
+			          </div>
+			        </div>
+			      </div>
+			    </div>
+
+			    <div class="item row">
+			      <div class="col-md-4 col-sm-6 col-xs-12">
+			        <div class="thingsBox">
+			          <div class="thingsImage">
+			            <img src="img/team/rui.png" alt="Rui Varela" height="350" width="500">
+			          </div>
+			          <div class="thingsCaption">
+			            <ul class="list-inline captionItem">
+			              <li><i class="fa" aria-hidden="true"></i> Rui Varela</li>
+			              <li><a href="https://www.linkedin.com/in/rui-rodrigues-varela/" target="_blank">LinkedIn</a></li>
+			            </ul>
+			          </div>
+			        </div>
+			      </div>
+			    </div>
+
+			    <div class="item row">
+			      <div class="col-md-4 col-sm-6 col-xs-12">
+			        <div class="thingsBox">
+			          <div class="thingsImage">
+			            <img src="img/team/sofia.png" alt="Sofia Gonçalves" height="350" width="500">
+			          </div>
+			          <div class="thingsCaption">
+			            <ul class="list-inline captionItem">
+			              <li><i class="fa" aria-hidden="true"></i> Sofia Gonçalves</li>
+			              <li><a href="https://www.linkedin.com/in/sofia-gonçalves-7aa753111/" target="_blank">LinkedIn</a></li>
+			            </ul>
+			          </div>
+			        </div>
+			      </div>
+			    </div>
+
+			  </div>
+			  <a class="left carousel-control" href="#thubmnailSlider" data-slide="prev"><i class="fa fa-angle-left" aria-hidden="true"></i></a>
+			  <a class="right carousel-control" href="#thubmnailSlider" data-slide="next"><i class="fa fa-angle-right" aria-hidden="true"></i></a>
+			</div>
 			</div>
 		</div>
 	</div>
