ec2a78c28f5d7b55cd499a0257bee98135fd5a0c
Patrick Fonseca <patrick.fcf@gmail.com>
2018-05-14 17:49:59 +0100

    Fixed monitoring class indentions
    Fixed monitoring class indentions

--- beta/php/rollbar.php
+++ beta/php/rollbar.php
@@ -1,15 +1,15 @@
 <?php
 
-use \Rollbar\Rollbar;
-use \Rollbar\Payload\Level;
-Rollbar::init(
-	array(
-		'access_token' => '9edcf88104cd4974bbdd498465bb15f4',
-		'environment' => 'development'
-	)
-);
+	use \Rollbar\Rollbar;
+	use \Rollbar\Payload\Level;
+	Rollbar::init(
+		array(
+			'access_token' => '9edcf88104cd4974bbdd498465bb15f4',
+			'environment' => 'development'
+		)
+	);
 
-Rollbar::log(Level::info(), 'Test info message');
-throw new Exception('Test exception');
+	Rollbar::log(Level::info(), 'Test info message');
+	throw new Exception('Test exception');
 
 ?>
